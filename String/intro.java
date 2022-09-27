675abda8ef74ba5db575f8a4af2f5081852ff4b6
Krishna Nand Patel <imkrishna8737@gmail.com>
2022-09-27 18:14:43 +0530

    String started
    Signed-off-by: Krishna Nand Patel <imkrishna8737@gmail.com>

---
+++ String/intro.java
@@ -0,0 +1,19 @@
+package String;
+
+import java.util.Scanner;
+
+public class intro{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        // This gets input till the blank space
+        String s1=scn.next();
+        // This gets input till next line
+        String s2=scn.nextLine();
+        System.out.println(s1);
+        //For getting a character at a definite position
+        char b=s2.charAt(1);
+        System.out.println(b);
+        // This will close the scanner and save resource
+        scn.close();
+    }
+}
